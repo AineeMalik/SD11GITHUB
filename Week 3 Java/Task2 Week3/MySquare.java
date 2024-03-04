b2523d8edeb981150b3b22c0ca52ff91ef3b9c4d
AineeMalik <117207213+AineeMalik@users.noreply.github.com>
2024-03-04 09:08:45 -0500

    Add files via upload

---
+++ Week 3 Java/Task2 Week3/MySquare.java
@@ -0,0 +1,9 @@
+public class MySquare {
+    //add vertices
+
+    //add constructors
+    
+    //toString
+
+    //perimeter and area methods
+}
---
+++ Week 3 Java/Task2 Week3/test.java
@@ -0,0 +1,12 @@
+public class text {
+    public static void main(String[] args) {
+        MyPoint p1 = new MyPoint(5, 5);
+        MyPoint p2 = new MyPoint(5, 5);
+
+        System.out.println(p1);
+
+        System.out.println(p1.distance(p2));
+        System.out.println(p1.distance());
+
+    }
+}
