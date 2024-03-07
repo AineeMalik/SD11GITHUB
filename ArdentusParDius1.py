169e7e3200ad327a27a9cbd0fa8cf77751b52c76
ArdentPardy <162140854+ArdentPardy@users.noreply.github.com>
2024-03-07 11:40:39 -0330

    Add files via upload
    Just a bit of folderol to see if I am understanding Git properly (have a look, SD11 and tell me if it works for you and if it has any value0

---
+++ ArdentusParDius1.py
@@ -0,0 +1,8 @@
+# Desc/Purp - Testing My GitHub Skills & sharing a Python tip (& having some fun)
+# Auth      - Ardent Pardy
+# Date(s)   - 2024-03-07, 
+
+print("\nObi-Wan (dropping in from the ceiling): 'Hello There.'")
+# uses a little trick ChatGPT showed me - the \n causes the text to appear on a new line
+print("\nGrievous (whirling around): 'General Kenobi!'") 
+print("\n;o)")
